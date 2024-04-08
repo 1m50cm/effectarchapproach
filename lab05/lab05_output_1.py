919ae2443b18c604e3db885a1b62bccfaa6817a5
1m50cm <84292315+1m50cm@users.noreply.github.com>
2024-04-08 20:57:05 +0200

    Create lab05_output_1.py

---
+++ lab05/lab05_output_1.py
@@ -0,0 +1,23 @@
+class Product:
+    def __init__(self, name, price, pr_type):
+        self.name = name
+        self.price = price
+        self.type = pr_type
+
+
+class ProductSearch:
+    def search_product(self, product, query):
+        # Пошук товару за запитом
+        pass
+
+
+class ProductDisplay:
+    def display_product(self, product):
+        # Відображення інформації про товар
+        pass
+
+
+class ProductOrder:
+    def order_product(self, prodict, quantity):
+        # Замовлення товару
+        pass
