10d9b1f366483777857942392d48ec4a0ae8da50
1m50cm <84292315+1m50cm@users.noreply.github.com>
2024-05-27 16:58:17 +0200

    Update lab06_output_1.py

--- lab06/lab06_output_1.py
+++ lab06/lab06_output_1.py
@@ -1,1 +1,14 @@
+def calculate_total_price(product_prices, discount):
+    total_price = 0
+    for price in product_prices:
+        if discount:
+            total_price += price * 0.9  # 10% discount
+        else:
+            total_price += price
+    return total_price
+
+
+def add_tax(price, tax_rate):
+    price *= (1 + tax_rate)
+    return price
 
